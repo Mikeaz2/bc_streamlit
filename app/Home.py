7d4352b30702eb3cdd3f9e5dfc04b2bd14347838
Mikeaz2 <mi089960@ucf.edu>
2025-11-21 20:38:32 -0500

    Update Home.py

--- app/Home.py
+++ app/Home.py
@@ -63,8 +63,12 @@ st.markdown(
 header_logo_col, header_text_col, _ = st.columns([0.6, 3, 0.5])
 
 with header_logo_col:
-    # bigger logo
-    st.image("app/assets/bc-logo.png", width=250)
+    st.markdown(
+        """
+        <img src="app/assets/bc-logo.png" width="120" style="margin-top:-12px;">
+        """,
+        unsafe_allow_html=True
+    )
 
 with header_text_col:
     st.markdown('<div class="bc-header-title">BC – Borderless Credit</div>', unsafe_allow_html=True)
